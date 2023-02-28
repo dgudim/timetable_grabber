c4125d575560bf54eb7a09ab03067a7ed69887f8
kloud-sauron <dgudim@gmail.com>
2023-02-28 17:59:16 +0200

    Better messages, comparison by day

--- index.ts
+++ index.ts
@@ -70,12 +70,15 @@ fs.writeFileSync('./cookies.json', JSON.stringify(cookies, null, 2));
 console.log("Saved cookies");
 
 // wait for main page to load
-await page.waitForSelector(".main_logo");
+await page.waitForSelector(".week");
+
+const week = await page.evaluate(() => document.querySelector(".week")?.textContent?.trim().replace("week ", ""));
+console.log(`Current week: ${week}`);
 
 // call timetable endpoint
 await page.goto("https://mano.vilniustech.lt/timetable/site/my-timetable");
 
-const timetable = await page.$eval('*', (el) => {
+const timetable_raw = await page.$eval('*', (el) => {
     const selection = window.getSelection();
     const range = document.createRange();
     range.selectNode(el);
@@ -84,54 +87,135 @@ const timetable = await page.$eval('*', (el) => {
     return window.getSelection()?.toString();
 });
 
-if (fs.existsSync("./timetable.txt")) {
-    if (fs.readFileSync("./timetable.txt").toString() != timetable) {
-        await fetch('https://ntfy.sh/mano_vilniustech_timetable_changed', {
-            method: 'POST',
-            body: 'Update google calendar accordingly',
-            headers: { 'Tags': 'warning,orange_square', 'Title': 'Vilniustech timetable changed' }
-        })
-    } else {
-        await fetch('https://ntfy.sh/mano_vilniustech_timetable_changed', {
-            method: 'POST',
-            body: 'All good',
-            headers: { 'Tags': 'green_square', 'Title': 'Vilniustech timetable scrub done' }
-        })
-    }
-}
-
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
-if (timetable) {
+type Subject = {
+    index: string,
+    time: string,
+    week: string,
+    group: string,
+    name_and_type: string,
+    auditorium: string
+}
+
+function stringify(obj: any): string {
+    return JSON.stringify(obj);
+}
+
+function objectsEqual(obj1: any, obj2: any): boolean {
+    return stringify(obj1) === stringify(obj2);
+}
+
+if (timetable_raw) {
 
     const searchStr = "Lecture\tTime\tWeek\tSubgroup\tSubject\tAuditorium\tLecturer\tType";
-    const indexes = [...timetable.matchAll(new RegExp(searchStr, 'gi'))].map(a => a.index);
+    const indexes = [...timetable_raw.matchAll(new RegExp(searchStr, 'gi'))].map(a => a.index);
 
-    let i: number = 0;
-    let timetable_map = new Map;
+    console.log('Current timetable:');
+
+    let i = 0;
+    let timetable_map: Map<string, Subject[]> = new Map();
     for (const index of indexes) {
-        const last_index = timetable.indexOf("\\n", index);
-        const sub_timetable = timetable.substring(index! + searchStr.length, last_index).trim();
-        const sub_timetable_entries_sep = sub_timetable.split("\n");
+        const last_index = timetable_raw.indexOf("\\n", index);
+        const day_timetable = timetable_raw.substring(index! + searchStr.length, last_index).trim();
+        const day_timetable_subjects_sep = day_timetable.split("\n");
         let sub_timetable_entries = [];
-        for (let i = 0; i < sub_timetable_entries_sep.length; i += 3) {
+        for (let i = 0; i < day_timetable_subjects_sep.length; i += 3) {
+            // merge lecurer and type with the entry
             sub_timetable_entries.push(
-                sub_timetable_entries_sep[i].trim() + "\t" +
-                sub_timetable_entries_sep[i + 1].trim() + "\t" +
-                sub_timetable_entries_sep[i + 2].trim());
+                day_timetable_subjects_sep[i].trim() + "\t" +
+                day_timetable_subjects_sep[i + 1].trim() + "\t" +
+                day_timetable_subjects_sep[i + 2].trim());
         }
-        let sub_timetable_subjects = [];
+        let day_timetable_subjects: Subject[] = [];
         for (const entry of sub_timetable_entries) {
-            sub_timetable_subjects.push(entry.split("\t"));
+            const subject_raw = entry.split("\t");
+
+            let type = "";
+
+            switch (subject_raw[7]) {
+                case "Lectures":
+                    type = " (lecture)";
+                    break;
+                case "Practical exercises (practical work)":
+                    type = " (lecture)";
+                    break;
+                case "Laboratory work (laboratory works)":
+                    type = " (lab work)";
+                    break;
+            }
+
+            const subject: Subject = {
+                index: subject_raw[0],
+                time: subject_raw[1],
+                week: subject_raw[2],
+                group: subject_raw[3],
+                name_and_type: subject_raw[4].substring(0, subject_raw[4].indexOf("(") - 1) + type,
+                auditorium: subject_raw[5].substring(0, subject_raw[5].indexOf("Auditorium"))
+            };
+
+            if (subject.group == "0" || subject.group == creds.group) {
+                day_timetable_subjects.push(subject);
+            } else {
+                console.log(`Skipped ${stringify(subject)}`);
+            }
+
         }
         console.log(days[i]);
-        console.log(sub_timetable_subjects);
-        timetable_map.set(days[i], sub_timetable_subjects);
+        console.log(day_timetable_subjects);
+        timetable_map.set(days[i], day_timetable_subjects);
         i++;
     }
 
-    fs.writeFileSync('./timetable.txt', JSON.stringify(timetable_map));
+    if (fs.existsSync("./timetable.txt")) {
 
+        const timetable_map_old: Map<string, Subject[]> = new Map(JSON.parse(fs.readFileSync("./timetable.txt").toString()));
+
+        let changed: number = 0;
+
+        for (const day of days) {
+            const old_subjects = timetable_map_old.get(day) || [];
+            const new_subjects = timetable_map.get(day) || [];
+
+            const removed = old_subjects.filter((old_sub: Subject) => !new_subjects.some(new_sub => objectsEqual(old_sub, new_sub)));
+            const added = new_subjects.filter((new_sub: Subject) => !old_subjects.some(old_sub => objectsEqual(old_sub, new_sub)));
+
+            if (removed.length == 0 && added.length == 0) {
+                console.log(`No changes on ${day}`);
+                continue;
+            }
+
+            changed += removed.length + added.length;
+
+            for (const rem_subject of removed) {
+                await fetch('https://ntfy.sh/mano_vilniustech_timetable_changed', {
+                    method: 'POST',
+                    body: `${rem_entry[1]} | ${rem_entry[4]} removed`,
+                    headers: { 'Tags': 'orange_square', 'Title': `${day} timetable changed` }
+                });
+                console.log(`added ${stringify(rem_subject)}`);
+            }
+
+            for (const add_subject of added) {
+                await fetch('https://ntfy.sh/mano_vilniustech_timetable_changed', {
+                    method: 'POST',
+                    body: `${add_entry[1]} | ${add_entry[4]} added`,
+                    headers: { 'Tags': 'orange_square', 'Title': `${day} timetable changed` }
+                });
+                console.log(`added ${stringify(add_subject)}`);
+            }
+        }
+
+        await fetch('https://ntfy.sh/mano_vilniustech_timetable_changed', {
+            method: 'POST',
+            body: `${changed} entries changed`,
+            headers: { 'Tags': 'green_square', 'Title': 'Timetable scrape done' }
+        })
+        console.log(`Scrape done, ${changed} entries changed`);
+
+    }
+
+    fs.writeFileSync('./timetable.txt', JSON.stringify(Array.from(timetable_map.entries())));
 }
 
 console.log("DONE");
